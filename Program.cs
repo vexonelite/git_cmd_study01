ddf5db0cdfb9b27b0c608d11ec98592796da74a9
elite_lin <elite_lin@goglobal.com.tw>
2021-01-21 10:51:07 +0800

    ready to test conflict #001

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ namespace HelloDotnet
     {
         static void Main(string[] args)
         {
+        	Console.WriteLine("2021/01/21");
             Console.WriteLine("Hello Dotnet!");
         }
     }
