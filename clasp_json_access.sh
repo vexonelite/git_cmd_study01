fbfd66158104d6f6f3871f1f2802573525d73978
Vexon Elite <vexonelit@gmail.com>
2022-08-23 13:52:38 +0800

    remove  $HOME/

--- clasp_json_access.sh
+++ clasp_json_access.sh
@@ -5,5 +5,5 @@ mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$CLASP_JSON_USAGE" \
---output $HOME/.clasprc.json $HOME/.clasprc.json.gpg
+--output $HOME/.clasprc.json .clasprc.json.gpg
 
